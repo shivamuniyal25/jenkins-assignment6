2f9d2881c00ece355e6514e48bd8e5be07e0723f
Shivam <shivamuniyal20@gmail.com>
2026-01-12 18:54:32 +0530

    Update ansiblePipeline.groovy

--- vars/ansiblePipeline.groovy
+++ vars/ansiblePipeline.groovy
@@ -1,1 +1,24 @@
+def call(Map config = [:]) {
 
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Clone Repository') {
+                steps {
+                    echo "Cloning repository..."
+                    git branch: 'main',
+                        url: config.REPO_URL
+                }
+            }
+
+            stage('Info') {
+                steps {
+                    echo "Environment: ${config.ENVIRONMENT}"
+                    echo "Code base path: ${config.CODE_BASE_PATH}"
+                }
+            }
+        }
+    }
+}
